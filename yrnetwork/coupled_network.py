d095e887baba5878e385040ccb4d1a072a029343
THRILLERLEMON <thrillerlemon@outlook.com>
2020-11-19 11:29:53 +0800

    添加了需要数据的描述

--- yrnetwork/coupled_network.py
+++ yrnetwork/coupled_network.py
@@ -1,2 +1,15 @@
 #TODO THRILLER:多要素构建网络
 #TODO THRILLER:需要实现不同时间段不同时间尺度的关系识别
+
+
+# fake code
+def build_coupled_net(p_A_var, p_B_var, p_time_scale, p_time_start,
+                      p_time_end):
+    """
+    build a coupled net for A and B
+    p_time_scale='yearly'
+    p_time_start,p_time_end like 1986 and 2018
+    p_time_scale='yearly'
+    p_time_start,p_time_end like 198601 and 201812
+    """
+    pass
