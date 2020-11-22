c2699074ab98fd2e4ec055cbaab4d4dfcce9aa89
THRILLERLEMON <thrillerlemon@outlook.com>
2020-11-22 14:57:29 +0800

    更新setting中的示例数据路径

--- yrnetwork/setting.py
+++ yrnetwork/setting.py
@@ -1,8 +1,9 @@
 class BaseConfig(object):
     RESULT_PATH = ''
     LUC_NET_DATA_PATH = 'D://OneDrive//JustDo//The_Greening_of_YR_from_the_Perspective_of_Network//Data//LUCNetData//'
-    LUC_NET_DATA_PATH_BIG = 'D://OneDrive//JustDo//The_Greening_of_YR_from_the_Perspective_of_Network//Data//BigClass_InfoForLUCNet//'
     LUC_NET_DATA_HEAD = 'InfoForLUCNet'
-    LUC_NET_DATA_HEAD_BIG = 'BigClass_InfoForLUCNet'
     LUC_NET_DATA_TAIL = '.csv'
+    COUPLED_NET_DATA_PATH = 'E://MY PROGRAM//YR_Greening_Network//data//SampleData//'
+    COUPLED_NET_DATA_HEAD = 'YR_GA_Info_'
+    COUPLED_NET_DATA_TAIL = '.csv'
     OUT_PATH = 'D://OneDrive//JustDo//The_Greening_of_YR_from_the_Perspective_of_Network//OutPut//'
